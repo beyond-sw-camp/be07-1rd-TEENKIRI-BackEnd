da3ac9b4f18997893d0159336eb3e5994ed084c2
milcho0604 <128167167+milcho0604@users.noreply.github.com>
2024-08-25 16:13:49 +0900

    Delete teenkiri_front directory

--- teenkiri_front/teenkiri_front/src/router/reportRouter.js
+++
@@ -1,9 +0,0 @@
-import ReportList from "@/views/report/ReportList.vue";
-
-export const reportRouter =[
-    {
-        path: '/report/list',
-        name: 'ReportList',
-        component: ReportList
-    },
-]
